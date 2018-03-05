dcb329dafce357850aa91970bd4533725c660712
Nazeebo <romanov0902@gmail.com>
2018-03-05 23:59:55 +0200

    Update listener.cpp

--- lab_8/2_inheritance/listener.cpp
+++ lab_8/2_inheritance/listener.cpp
@@ -7,36 +7,30 @@
 
 char Listener::letterInForClass() {
     char choice;
-    bool flag = false;
     while (1) {
         scanf("%c", &choice);
         if (choice == 'K' || choice == 'M' || choice == 'G')
-            flag = true;
-        if (flag) break;
+            break;
     }
     return choice;
 }
 
 char Listener::letterInForAction() {
     char choice;
-    bool flag = false;
     while (1) {
         scanf("%c", &choice);
         if (choice == 'A' || choice == 'S' || choice == 'M')
-            flag = true;
-        if (flag) break;
+            break;
     }
     return choice;
 }
 
 char Listener::letterInForWay() {
     char choice;
-    bool flag = false;
     while (1) {
         scanf("%c", &choice);
         if (choice == 'F' || choice == 'B')
-            flag = true;
-        if (flag) break;
+            break;
     }
     return choice;
 }
